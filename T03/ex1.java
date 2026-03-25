f89c1b5fd1d148d70712bcab1527540d08f034bf
Goomes11 <gomespessoal2616@gmail.com>
2026-03-25 18:16:16 -0300

    atividades T01 a T04
    Foram incluídas as atividades T01, T02, T03 e T04 no repositório.
    
    Cada pasta contém:
    - Arquivo .java com a implementação da resolução
    - Arquivo .pdf contendo:
      - Resolução detalhada
      - Fluxograma
      - Explicação do desenvolvimento
    
    As atividades foram organizadas individualmente em suas respectivas pastas para melhor estruturação e compreensão.

---
+++ T03/ex1.java
@@ -0,0 +1,12 @@
+public class ex1 {
+    public static void main(String[] args) {
+
+        String nome = "Ryan";
+        int idade = 20;
+        double altura = 1.75;
+
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade + " anos");
+        System.out.println("Altura: " + altura + " m");
+    }
+}
